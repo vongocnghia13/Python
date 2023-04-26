e9a7424e2690ac2d0c20034c9f4a5a95224d0414
vongocnghia13 <127461295+vongocnghia13@users.noreply.github.com>
2023-04-26 23:21:10 +0700

    Add files via upload

---
+++ VoNgocNghia.FOR.py/bai1.py
@@ -0,0 +1,3 @@
+n = int(input("Nhập vào số n: "))
+while n <= 0 or n >= 100:
+    n = int(input("Số bạn nhập không hợp lệ, vui lòng nhập lại: "))
---
+++ VoNgocNghia.FOR.py/bai2.py
@@ -0,0 +1,2 @@
+numbers = [50, 2019, 53, 56, 60]
+print(numbers[1]) # In ra số thứ hai trong danh sách (index bắt đầu từ 0)
